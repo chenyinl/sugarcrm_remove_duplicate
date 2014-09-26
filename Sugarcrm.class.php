2bafcbdf7f97ac3a655fd3bbb522f0541f649580
Chen Lin <chenyinl@yahoo.com>
2014-09-26 16:50:13 -0700

    checked in

---
+++ Sugarcrm.class.php
@@ -0,0 +1,375 @@
+<?php
+require_once( "sugarcrm.config.php" );
+
+class Sugarcrm{
+
+    /* received from login process, like token */
+    private $session_id = false;
+
+    // user id from the search result
+    public $lead_id = false;
+
+    public $lead_status = false;
+
+    // keep the user id after add new user. For test only
+    public $user_id = false;
+
+    public function getAllLead ()
+    {
+        if(! $this->session_id) $this->login();
+        $get_entry_list_parameters = array(
+
+            //session id
+            'session' => $this->session_id,
+
+            //The name of the module from which to retrieve records
+            'module_name' => 'Leads',
+
+            //The SQL WHERE clause without the word "where".
+            'query' => "",
+
+            //The SQL ORDER BY clause without the phrase "order by".
+            'order_by' => "",
+
+            //The record offset from which to start.
+            'offset' => '0',
+
+            //Optional. A list of fields to include in the results.
+            'select_fields' => array(
+                'id',
+                'email1',
+            ),
+
+         /*
+         A list of link names and the fields to be returned for each link name.
+         Example: 'link_name_to_fields_array' => array(array('name' => 'email_addresses', 'value' => array('id', 'email_address', 'opt_out', 'primary_address')))
+         */
+            'link_name_to_fields_array' => array(),
+
+            //The maximum number of results to return.
+            'max_results' => 2000,
+
+            //To exclude deleted records
+            'deleted' => '0',
+
+            //If only records marked as favorites should be returned.
+            'Favorites' => false,
+        );
+
+        return $this->call('get_entry_list', $get_entry_list_parameters);
+    }
+    public function search( $string ){
+        if(! $this->session_id) $this->login();
+        $search_by_module_parameters = array(
+            //Session id
+            "session" => $this->session_id,
+
+            //The string to search for.
+            'search_string' => $string,
+
+            //The list of modules to query.
+            'modules' => array('Leads'),
+            //'modules' => 'Leads',
+
+            //The record offset from which to start.
+            'offset' => 0,
+
+            //The maximum number of records to return.
+            'max_results' => 1,
+
+            //Filters records by the assigned user ID.
+            //Leave this empty if no filter should be applied.
+            'id' => '',
+
+            //An array of fields to return.
+            //If empty the default return fields will be from the active listviewdefs.
+            //'select_fields' =>array(),
+            
+            'select_fields' => array(
+            
+                'id',
+                'status',
+                'status_description',
+                'package_purchased_c'
+            ),
+
+            //If the search is to only search modules participating in the unified search.
+            //Unified search is the SugarCRM Global Search alternative to Full-Text Search.
+            'unified_search_only' => false,
+
+            //If only records marked as favorites should be returned.
+            'favorites' => false
+        );
+
+        $search_by_module_result = $this->call('search_by_module', $search_by_module_parameters);
+        var_dump($search_by_module_result);
+        //exit();
+        $resultArray = $search_by_module_result->entry_list[0]->records;
+        //var_dump($resultArray);
+        //return;
+        if(count($resultArray)!=0){
+            //var_dump($resultArray);
+            //ob_flush();
+            //flush();
+            // already registered
+            $this->lead_id = $resultArray[0]->id->value;
+            $this->lead_status = $resultArray[0]->status->value;
+            //$this->lead_package_purchased = $resultArray[0]->package_purchased->value;
+            //$this->lead_saffron_id = $resultArray[0]->saffron_id_c->value;
+            return true;
+        }else{
+            return false;
+        }
+        
+    }    
+    public function searchDuplicate( $string ){
+        if(! $this->session_id) $this->login();
+        $search_by_module_parameters = array(
+            //Session id
+            "session" => $this->session_id,
+
+            //The string to search for.
+            'search_string' => $string,
+
+            //The list of modules to query.
+            'modules' => array('Leads'),
+            //'modules' => 'Leads',
+
+            //The record offset from which to start.
+            'offset' => 0,
+
+            //The maximum number of records to return.
+            'max_results' => 10,
+
+            //Filters records by the assigned user ID.
+            //Leave this empty if no filter should be applied.
+            'id' => '',
+
+            //An array of fields to return.
+            //If empty the default return fields will be from the active listviewdefs.
+            //'select_fields' =>array(),
+            
+            'select_fields' => array(
+            
+                'id',
+                'status',
+                'status_description',
+                'package_purchased_c', 
+                'date_modified',
+                'date_entered'
+            ),
+
+            //If the search is to only search modules participating in the unified search.
+            //Unified search is the SugarCRM Global Search alternative to Full-Text Search.
+            'unified_search_only' => false,
+
+            //If only records marked as favorites should be returned.
+            'favorites' => false
+        );
+
+        $search_by_module_result = $this->call('search_by_module', $search_by_module_parameters);
+        if(count($search_by_module_result->entry_list[0]->records)>1){
+            return ($search_by_module_result->entry_list[0]->records);
+            //return true;
+        }else{
+            return FALSE;
+        }
+        //return($search_by_module_result);
+        //exit();
+        //$resultArray = $search_by_module_result->entry_list[0]->records;
+        //var_dump($resultArray);
+        //return;
+        /*
+        if(count($resultArray)!=0){            // already registered
+            $this->lead_id = $resultArray[0]->id->value;
+            $this->lead_status = $resultArray[0]->status->value;
+            return true;
+        }else{
+            return false;
+        }*/
+        
+    }    
+    private function call( $method, $parameters ){
+        //ob_start();
+        $curl_request = curl_init();
+
+        curl_setopt($curl_request, CURLOPT_URL, URL);
+        curl_setopt($curl_request, CURLOPT_POST, 1);
+        curl_setopt($curl_request, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($curl_request, CURLOPT_HEADER, 1);
+        curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_request, CURLOPT_FOLLOWLOCATION, 0);
+
+        $jsonEncodedData = json_encode($parameters);
+
+        $post = array(
+             "method" => $method,
+             "input_type" => "JSON",
+             "response_type" => "JSON",
+             "rest_data" => $jsonEncodedData
+        );
+
+        curl_setopt($curl_request, CURLOPT_POSTFIELDS, $post);
+        $result = curl_exec($curl_request);
+        //echo "Call result: ".$result."\n";
+        curl_close($curl_request);
+
+        $result = explode("\r\n\r\n", $result, 2);
+        $response = json_decode($result[1]);
+        //ob_end_flush();
+
+        return $response;        
+    } 
+    public function login()
+    {
+        $login_parameters = array(
+            "user_auth" => array(
+                "user_name" => USERNAME,
+                "password" => md5( PASSWORD ),
+                "version" => "1"
+            ),
+            "application_name" => "RestTest",
+            "name_value_list" => array()
+        );
+
+        $login_result = $this->call("login", $login_parameters);
+        $this->session_id = $login_result->id;
+        return true;
+    }
+
+    public function addNew( 
+        $first_name, 
+        $last_name, 
+        $lead_source_description, 
+        $status_description, 
+        $email, 
+        $status, 
+        $assigned_user_id 
+    ){
+        if( !$this->session_id ) $this->login();
+        $set_entry_parameters = array(
+            //session id
+            "session" => $this->session_id,
+
+            //The name of the module from which to retrieve records.
+            "module_name" => "Leads",
+
+            //Record attributes
+            "name_value_list" => array(
+                array("name" => "last_name", "value" => $last_name),
+                array("name" => "first_name", "value" => $first_name),
+                array("name" => "email1", "value" => $email),
+                array("name" => "status", "value" => $status),
+                array("name" => "status_description", "value" => $status_description),
+                array("name" => "lead_source_description", "value" => $lead_source_description),
+                array("name" => "assigned_user_id", "value" => $assigned_user_id),
+                array("name" => "campaign_id", "value" => CAMPAIGN_ID)
+            )
+        );
+
+        $set_entry_result = $this->call("set_entry", $set_entry_parameters);
+        $this->user_id = $set_entry_result->id;
+        return true;
+    }
+
+    public function updateLeadStatus( $id, $status, $assigned_user_id )
+    {
+        //if( !$this->session_id ) $this->login();
+        $set_entry_parameters = array(
+            //session id
+            "session" => $this->session_id,
+
+            //The name of the module from which to retrieve records.
+            "module_name" => "Leads",
+
+            //Record attributes
+            "name_value_list" => array(
+                array("name" => "id", "value" => $id),
+                array("name" => "assigned_user_id", "value" => $assigned_user_id),
+                array("name" => "status", "value" => $status),
+            )
+        );
+
+        $set_entry_result = $this->call("set_entry", $set_entry_parameters);
+        return true;
+    }
+    public function updatePackagePurachsed( $id, $plan )
+    {
+        $set_entry_parameters = array(
+            //session id
+            "session" => $this->session_id,
+
+            //The name of the module from which to retrieve records.
+            "module_name" => "Leads",
+
+            //Record attributes
+            "name_value_list" => array(
+                array("name" => "id", "value" => $id),
+                array("name" => "package_purchased_c", "value" => $plan)
+            )
+        );
+
+        $set_entry_result = $this->call("set_entry", $set_entry_parameters);
+        return true;
+    }
+    public function updatePackage_UTM_Campaign( $id, $plan, $utm, $campaign_id )
+    {
+        $set_entry_parameters = array(
+            //session id
+            "session" => $this->session_id,
+
+            //The name of the module from which to retrieve records.
+            "module_name" => "Leads",
+
+            //Record attributes
+            "name_value_list" => array(
+                array("name" => "id", "value" => $id),
+                array("name" => "package_purchased_c", "value" => $plan),
+                array("name" => "utm_campaign_c", "value"=> $utm),
+                array("name" => "campaign_id", "value"=> $campaign_id)
+            )
+        );
+
+        $set_entry_result = $this->call("set_entry", $set_entry_parameters);
+        var_dump($set_entry_result );
+        return true;
+    }
+    public function searchById( $id )
+    {
+        $get_entries_parameters = array(
+             //session id
+             'session' => $this->session_id,
+             //The name of the module from which to retrieve records
+             'module_name' => 'Leads',
+             //An array of record IDs
+             'ids' => array(
+                 $id,
+             ),
+             //The list of fields to be returned in the results
+             'select_fields' => array(
+                'id',
+                'package_purchased_c'
+                //'name',
+                //'billing_address_state',
+                //'billing_address_country'
+             ),
+             //A list of link names and the fields to be returned for each link name
+             'link_name_to_fields_array' => array(
+                  array(
+                       'name' => 'email_addresses',
+                       'value' => array(
+                            'email_address',
+                            'opt_out',
+                            'primary_address'
+                       ),
+                  ),
+             ),
+            //Flag the record as a recently viewed item
+            'track_view' => false,
+        );
+        $set_entry_result = $this->call("get_entries", $get_entries_parameters);
+        var_dump($set_entry_result);
+        return true;
+    }
+}
---
+++ findDuplicate.php
@@ -0,0 +1,134 @@
+#!/Applications/MAMP/bin/php/php5.4.10/bin/php
+<?php
+
+//This is used to update UTM and plan code on Production 
+// Chen Lin
+// 09/23/2014
+
+define("CAMPAIGN_ID_SUBSCRIBER", "1722198f-b731-240c-ab87-541c80da3b3c");
+if( !isset($argv[1]) || !isset($argv[2]) ){
+    echo "Missing file parameters.\nUsage:./findDuplicate.php Good.csv limit\n";
+    die();
+}
+if( !is_file( $argv[1] ) ){
+    echo "Faile to open file ".$argv[1]."\n";
+    exit();
+}
+
+if( !( ((STRING)(INT)$argv[2] )== $argv[2])){
+    echo "The second parameters ".$argv[2]." has to be integer; you put ".gettype($argv[2])."\n";
+    exit();
+}
+
+$filename = $argv[1];
+$maxrow = 100000;
+if( ( (INT)$argv[2] != -1 )){
+    $maxrow = $argv[2];
+}
+// try to open file to read
+try{
+    if (($handle = fopen($filename, "r")) === FALSE) {
+        echo "Faile to open file\n";
+        exit();
+    }
+} catch(Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+$row = 1;
+$planCount=array();
+    $planCount[0]=0;
+    $planCount[1]=0;
+    $planCount[2]=0;
+    $planCount[3]=0;
+   include_once( "Sugarcrm.class.php" );
+    include_once( "LocalDB.class.php" );
+$success=true; //the success data
+    $sc = new Sugarcrm();
+        //$sc -> login();
+
+        $count = 0;
+        //$data = fgetcsv($handle, 1000, ",");
+
+        $rowData = fgetcsv($handle, 1000, "\t"); //remove first line
+        $sc = new Sugarcrm();
+        $sc->login();
+        // use local database
+        $db = new LocalDB();
+        $db -> readDB();
+        $localData = $db->data;
+
+while (( $tempData = fgetcsv($handle, 1000, "\t")) !== FALSE && $count++ < $maxrow) {
+    //var_dump($tempData);
+            if(!($tempData[0])) continue;
+            list(
+                $saffron_id,
+                //$external_id  
+                $email,
+                $attributes,
+                //$password,
+                $window_id,
+                $status
+            ) = $tempData;
+            
+
+            //$saffron_id = str_replace("\n", "", $saffron_id);
+            $attributes = json_decode($attributes);
+
+            $utm = $attributes->utm_campaign;
+            if( $success){
+                switch( $window_id ){
+                    case ( "d611a7e7-a767-4bc3-9bfa-65dc079e0f57" ):
+                        $plan = 1;
+                        $planCount[1]++;
+                        break;
+                    case ( "da320f02-2d56-4833-b0c7-b1d1fbc01a53" ):
+                        $plan = 2;
+                        $planCount[2]++;
+                        break;
+                    case ( "ac882d72-e89d-4cc8-bea3-7536b7539551" ):
+                        $plan = 3;
+                        $planCount[3]++;
+                        break;
+                }
+                // not pulling from attribute
+                //$plan = $attributes->price_selection;
+            } else {
+                $plan = 0;
+                $planCount[0]++;
+            }
+            $result = $sc->searchDuplicate( $email );
+            if($result === FALSE){
+                echo $email." - has only one record.\n\n";
+            }else{
+                //var_dump($result);
+                $dup_count = count($result);
+                if( $dup_count==2) {
+                    if(($result[0]->date_entered->value) < ($result[1]->date_entered->value)){
+                        $old = $result[0];
+                        $new = $result[1];
+                    } else {
+                        $new = $result[0];
+                        $old = $result[1];
+                    }
+
+                    echo $email." - found ".$dup_count." records\n".
+                    "old: ".$old->id->value." ".$old->date_entered->value."\n".
+                    "new: ".$new->id->value." ".$new->date_entered->value."\n\n";
+                } else{
+                    echo $email." - found 2+ records\n";
+                }
+            }
+            //$lead_id = $sc->lead_id;
+
+            
+            
+            //$sc->updatePackage_UTM_Campaign ($lead_id, $window_id, $utm, CAMPAIGN_ID_SUBSCRIBER);
+            
+            //echo $row.". find ". $email." - ".$lead_id." with plan ".$window_id." CRM id: ".$source."\n";
+            $row++;   
+        }
+fclose( $handle );
+
+
+
